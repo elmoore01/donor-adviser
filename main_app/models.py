8a0374c439e4eb3f59f1365868f747020668d9d8
Eric Moore <eric@educate-ca.org>
2020-10-05 17:40:14 -0700

    Update Categories

--- main_app/models.py
+++ main_app/models.py
@@ -29,17 +29,17 @@ STATES = (
 
 CATAGORIES = (
     ('AG', 'Aging'),('AF', 'Agriculture and Food'),('AC', 'Arts and Culture'),
-    ('AS', 'Athletics and Sports'),('CY', 'Children and Youth'),('AF', 'After School'), ('CS', 'Civil Society'),
+    ('AS', 'Athletics and Sports'),('CY', 'Children and Youth'),('AF', 'After School'), ('SU', 'Summer Activities'), ('CS', 'Civil Society'),
     ('CE', 'Community and Economic Development'),('CT', 'Computers and Technology'),('CP', 'Consumer Protection'),
     ('CR', 'Crime and Safety'),('DI', 'Disabilities'),('DO', 'Domestic Violence Prevention'),
     ('EL', 'Education and Literacy'), ('K2', 'K 12'), ('HE', 'Higher Education'), ('CR', 'Career Preparation'), ('AE', 'Adult Education'),('EM', 'Employment and Labor'),('EE', 'Energy and Environment'),
     ('LI', 'LGBTQ+'),('GR', 'Government Reform'),('HW', 'Health and Wellness'),
     ('HO', 'Housing and Homelessness'),('HR', 'Human Rights and Civil Liberties'),('HU', 'Hunger'),
     ('IM', 'Immigration'),('JM', 'Journalism and Media'),('MB', 'Men and Boys'),
-    ('NP', 'Nonprofit Empowerment'),('PF', 'Parenting and Families'),('PO', 'Poverty'),
+    ('NP', 'Nonprofit Empowerment'),('EC', 'Executive Coaching'), ('BD', 'Board Development'), ('PF', 'Parenting and Families'),('PO', 'Poverty'),
     ('PJ', 'Prison and Judicial Reform'),('RE', 'Race and Ethnicity'),('RI', 'Religion'),
     ('SC', 'Science, Technology, Engineering and Math'),('SA', 'Substance Abuse and Recovery'),('TR', 'Transportation'),
-    ('WE', 'Welfare and Public Assistance'),('WG', 'Woman and Girls')
+    ('WE', 'Welfare and Public Assistance'),('WG', 'Women and Girls')
 )
 
 class Organization(models.Model):
